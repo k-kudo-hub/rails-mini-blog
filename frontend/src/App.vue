5117d5233edb1b2effc7c2b097a89a51ceb9269b
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-30 22:03:45 +0900

    Created header component.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <Header />
+    
+  </div>
 </template>
+
+<script>
+import Header from './components/shared/Header.vue'
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    Header,
+  },
+}
+</script>
 
 <style>
 .bg-gold-500 {
---
+++ frontend/src/components/shared/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="h-12 w-full text-gold-500 shadow-md flex">
+    <div class="w-1/6 flex justify-center items-center">
+      <img :src="image.logo" height="30" width="30">
+    </div>
+    <div class="w-2/6 flex justify-between items-center">
+    </div>
+    <div class="w-3/6 flex justify-end items-center">
+      <p class="mr-2">hogehoge</p>
+      <div class="w-8 h-8 mr-2 border-2 rounded-3xl border-gold-500">
+        <img :src="image.logo" height="32" width="32">
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import site_logo from '../../assets/m-logo-sk.png'
+export default {
+  data(){
+    return {
+      image: {
+        logo: site_logo
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
