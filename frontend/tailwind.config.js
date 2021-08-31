f0a0395cf689c753c5dc7ae4029590bc43ed4976
k-kudo-hub <tongariisaibou@gmail.com>
2021-08-31 19:56:47 +0900

    Fixed tailwindCSS custom colors error.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,15 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      gold: {
-        default: '#CCC0AE',
-      },
-      silver: {
-        default: '#C4C4C4',
-      },
-    }
+    extend: {
+      colors: {
+        gold: {
+          default: '#CCC0AE',
+        },
+        silver: {
+          default: '#C4C4C4',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
