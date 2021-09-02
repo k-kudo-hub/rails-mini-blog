ca0a58bfee22abb70eedae7d9bc0c8b8765e058e
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-03 08:33:43 +0900

    Supports multiple languages

--- frontend/src/lang/en.js
+++ frontend/src/lang/en.js
@@ -1,4 +1,9 @@
 const messages = Object.freeze({
+  form: {
+    require: "required",
+    character_20: "20 characters or less",
+    character_8_password: "8 characters or more. Must use Alphabets and numbers"
+  },
   link: {
     blog: "blogs",
     trend: "trend",
@@ -9,6 +14,13 @@ const messages = Object.freeze({
     blogs: "Blogs",
     follow: "following",
     follower: "follower",
+  },
+  session: {
+    sign_up: "Sign up",
+    registration: "registration",
+    name: "name",
+    email: "email",
+    password: "password"
   }
 })
 
--- frontend/src/lang/ja.js
+++ frontend/src/lang/ja.js
@@ -1,4 +1,9 @@
 const messages = Object.freeze({
+  form: {
+    require: "必須",
+    character_20: "20文字以内",
+    character_8_password: "8文字以上・英数字混合"
+  },
   link: {
     blog: "ブログ",
     trend: "トレンド",
@@ -9,6 +14,13 @@ const messages = Object.freeze({
     blogs: "記事一覧",
     follow: "フォロー",
     follower: "フォロワー",
+  },
+  session: {
+    sign_up: "新規登録",
+    registration: "登録",
+    name: "ユーザー名",
+    email: "メールアドレス",
+    password: "パスワード"
   }
 })
 
