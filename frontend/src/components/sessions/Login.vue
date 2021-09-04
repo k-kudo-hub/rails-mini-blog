e3b710219b578f0223394315b85eb083f695cc84
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-04 10:30:35 +0900

    Developed the login page UI

--- frontend/src/components/sessions/Login.vue
+++ frontend/src/components/sessions/Login.vue
@@ -1,25 +1,38 @@
 <template>
-  <div class="w-full bg-white shadow-md relative p-2">
-    <h1 class="mb-2">ログイン</h1>
-    <div v-if="errors.length > 0">
-      <p v-for="(item, index) in errors" :key="index">{{ item }}</p>
+  <div class="w-full bg-white shadow-md relative p-5">
+    <h1 class="mb-4 text-lg">{{ $t("session.sign_in") }}</h1>
+    <div v-if="errors.common.length > 0" class="mb-3">
+      <p v-for="(item, index) in errors.common" :key="index" class="text-red-500">{{ item }}</p>
     </div>
     <form @submit.prevent>
-      <div class="flex flex-col mb-2">
-        <label for="">メールアドレス</label>
-        <input v-model="user.email" class="border" type="text">
+      <div class="flex flex-col mb-3">
+        <label for="email" class="mb-2 flex items-center">{{ $t("session.email") }}<require-label/></label>
+        <input v-model="user.email" :class="addErrorBorder(errors.email)" class="border-b h-10" type="text" name="email">
+        <template v-if="errors.email.length > 0">
+          <p v-for="(item, index) in errors.email" :key="index" class="text-red-500">{{ item }}</p>
+        </template>
       </div>
-      <div class="flex flex-col mb-2">
-        <label for="">パスワード</label>
-        <input v-model="user.password" class="border" type="password">
+      <div class="flex flex-col mb-3">
+        <label for="password" class="mb-2 flex items-center">パスワード<require-label/></label>
+        <input v-model="user.password" :class="addErrorBorder(errors.email)" class="border-b h-10" type="password" name="password">
+        <template v-if="errors.password.length > 0">
+          <p v-for="(item, index) in errors.password" :key="index" class="text-red-500">{{ item }}</p>
+        </template>
       </div>
-      <button @click="authenticateUser" type="submit">ログイン</button>
+      <div class="flex justify-end mr-4">
+        <button-default
+         @click="authenticateUser"
+         :text="$t('session.sign_in')"
+        />
+      </div>
     </form>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import RequireLabel from '../shared/RequireLabel.vue'
+import ButtonDefault from '../shared/ButtonDefault.vue'
 export default {
   data(){
     return {
@@ -27,33 +40,58 @@ export default {
         email: "",
         password: "",
       },
-      errors: []
+      errors: {
+        email: [],
+        password: [],
+        common: [],
+      }
     }
   },
+  components: {
+    RequireLabel,
+    ButtonDefault
+  },
   methods: {
+    authenticate(){
+      axios
+        .post('http://localhost:3000/api/v1/sessions.json', {
+          session: {
+            email: this.user.email,
+            password: this.user.password
+          }
+        })
+        .then(response => {
+          console.log(`ようこそ、${response.data}さん！`)
+          this.$router.push({
+            name: 'home'
+          })
+        })
+        .catch(error => {
+          console.log(error.response.data)
+          this.resetErrors()
+          this.errors.common.push(this.$t("form.wrong_message"))
+        })
+    },
     authenticateUser(){
       if(this.user.email && this.user.password){
-        axios
-          .post('http://localhost:3000/api/v1/sessions.json', {
-            session: {
-              email: this.user.email,
-              password: this.user.password
-            }
-          })
-          .then(response => {
-            console.log(`ようこそ、${response.data}さん！`)
-          })
-          .catch(error => {
-            console.log(error.response.data)
-            this.errors = ["パスワードかメールアドレスが間違っています。"]
-          })
+        this.authenticate();
       } else {
         this.inputValidation();
       }
     },
     inputValidation(){
-      if(!this.user.email){ this.errors.push('メールアドレスを入力してください。') }
-      if(!this.user.password){ this.errors.push('パスワードを入力してください。') }
+      this.resetErrors()
+      if(!this.user.email){ this.errors.email.push(this.$t("session.email") + this.$t("form.require_message")) }
+      if(!this.user.password){ this.errors.password.push(this.$t("session.password") + this.$t("form.require_message")) }
+    },
+    resetErrors(){
+      // FIXME: 初期化する関数に置き換えたい
+      this.errors = { common: [], email: [], password: [] }
+    },
+    addErrorBorder(array){
+      if(array.length > 0){
+        return "border-red-500"
+      }
     }
   }
 }
