5ca38fa6abc53a9c9d4dc683bbd09efe7155eddc
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-10 17:44:36 +0900

    Created a new button design and cut it out into components.

---
+++ frontend/src/components/shared/ButtonFilled.vue
@@ -0,0 +1,11 @@
+<template>
+  <button type="submit" class="border border-gold-500 text-white hover:text-gold-500 bg-gold-500 hover:bg-white py-1 px-3 rounded-2xl shadow-md">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: "",
+  }
+}
+</script>
