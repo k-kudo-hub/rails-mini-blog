c19245957829c8affb040db507664a523d061fe9
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-11 08:58:28 +0900

    Created API for updating user information

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
+    user = User.new(user_create_params)
     if user.save
       session[:user_id] = user.id
       render json: user.name, status: :created
@@ -17,12 +17,26 @@ class Api::V1::UsersController < ApplicationController
     render json: @current_user
   end
 
+  def update
+    user = User.find(@current_user.id)
+    if user.update(user_update_params)
+      response = { name: user.name, introduce: user.introduce, link: user.link }
+      render json: response, status: :created
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
   def destroy
   end
 
   private
 
-  def user_params
+  def user_create_params
     params.require(:user).permit(:name, :email, :password)
+  end
+  
+  def user_update_params
+    params.require(:user).permit(:name, :introduce, :link)
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, { format: 'json' } do
     namespace :v1 do
-      resources :users,    only: [:create, :show, :destroy]
+      resources :users,    only: [:create, :show, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
     end
   end
