bb5152e92859cc2aadc042682314449ea03fec03
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-11 09:09:03 +0900

    Cut out the bottom of the profile into components

---
+++ frontend/src/components/users/ProfileFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-end mr-4">
+    <button-default
+      @click="$emit('signOut')"
+      :text="$t('session.sign_out')"
+    />
+  </div>
+</template>
+
+<script>
+import ButtonDefault from '../shared/ButtonDefault.vue'
+export default {
+  components: {
+    ButtonDefault,
+  }
+}
+</script>
+
+<style>
+
+</style>
