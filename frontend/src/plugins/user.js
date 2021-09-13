d2a9d4f01e3406a9681f370ea525e5ef54c342a2
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-13 21:52:55 +0900

    Changed the cutout destination of common methods.

---
+++ frontend/src/plugins/user.js
@@ -0,0 +1,30 @@
+export function catchErrorMessages(errors, self){
+  // FIXME: Vuei18nを使用して動的に判別したい
+  errors.forEach((error) => {
+    if(error.match('Name | ユーザー名')){ 
+      self.errors.name.push(error)
+      return
+    }
+    if(error.match('Introduce | 自己紹介')){
+      self.errors.introduce.push(error)
+      return
+    }
+    if(error.match('Link | URL')){
+      self.errors.link.push(error)
+      return
+    }
+    if(error.match('Picture | プロフィール画像')){
+      self.errors.picture.push(error)
+      return
+    }
+    if(error.match('Cover | カバー画像')){
+      self.errors.cover.push(error)
+      return
+    }
+  })
+}
+
+export function resetErrors(self){
+  // FIXME: 初期化する関数に置き換えたい
+  self.errors = { name: [], introduce: [], link: [], picture: [], cover: [] }
+}
