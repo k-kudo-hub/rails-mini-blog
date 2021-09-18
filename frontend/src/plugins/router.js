19a42ad3587675862c66587cb6a71327eb13519f
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-18 21:14:00 +0900

    Routing and component creation

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -1,12 +1,18 @@
-import Home from '../components/Home.vue'
-import Profile from '../components/users/Profile.vue'
+import Home         from '../components/Home.vue'
+import Profile      from '../components/users/Profile.vue'
 import Registration from '../components/sessions/Registration.vue'
-import Login from '../components/sessions/Login.vue'
+import Login        from '../components/sessions/Login.vue'
+import BlogCreate   from '../components/blogs/BlogCreate.vue'
 const routes = [
   {
     component: Home,
     name: 'home',
     path: '/'
+  },
+  {
+    component: BlogCreate,
+    name: 'blog_create',
+    path: '/blog'
   },
   {
     component: Profile,
