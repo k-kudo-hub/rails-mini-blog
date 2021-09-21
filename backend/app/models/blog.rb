610a4704bc747ea53442e9ead1015a970ce2c6a7
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-21 19:16:24 +0900

    Update validation.

--- backend/app/models/blog.rb
+++ backend/app/models/blog.rb
@@ -11,6 +11,7 @@ class Blog < ApplicationRecord
   validates :subject,       presence:     true,
                             length:       { maximum: SUBJECT_MAXIMUM_LENGTH }
   validates :state_number,  numericality: { in: 0..2 }
+  validates :url,           uniqueness:   { message: 'URL生成で問題が発生しました。お手数ですが再度「保存する」を押してください。' }, on: :create
 
   def set_url()
     self.url = self.random_url
