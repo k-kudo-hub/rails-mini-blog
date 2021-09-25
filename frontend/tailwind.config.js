9d528d45840a637d320f1313d22209b5f899a8c9
k-kudo-hub <tongariisaibou@gmail.com>
2021-09-25 17:36:13 +0900

    Updated color settings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
     extend: {
       colors: {
         gold: {
-          300: '#fff267',
+          300: '#bb9f00',
           500: '#CCC0AE',
+          700: '#b1a798'
         },
         silver: {
           500: '#C4C4C4',
