de30e99ff4722753bcba269c7c88f5afc9e67f73
k-kudo-hub <tongariisaibou@gmail.com>
2021-10-05 08:51:49 +0900

    Prepared for blog function test.

--- backend/spec/factories/blogs.rb
+++ backend/spec/factories/blogs.rb
@@ -1,12 +1,11 @@
 FactoryBot.define do
+  require 'securerandom'
   factory :blog do
-    user { nil }
-    organization { nil }
-    subject { 'MyString' }
-
-    body { 'MyText' }
-    cover_image { 'MyString' }
-    state_number { 1 }
-    url { 'MyString' }
+    association  :user
+    subject      { Faker::Lorem.characters(number: 10) }
+    body         { Faker::Lorem.characters(number: 40) }
+    cover_image  { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/assets/images/dummy-header.jpg')) }
+    state_number { Faker::Number.between(from: 0, to: 3) }
+    url          { SecureRandom.hex(20) }
   end
 end
