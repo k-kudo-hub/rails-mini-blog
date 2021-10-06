be3694fbf5dcce3b5b685b6b473d05c70c292300
k-kudo-hub <tongariisaibou@gmail.com>
2021-10-06 14:40:47 +0900

    Developed validation for evaluation.

--- backend/app/models/star.rb
+++ backend/app/models/star.rb
@@ -1,4 +1,17 @@
 class Star < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+
+  validate :check_own_star
+  validate :check_unreleased_blog
+
+  # 自分のブログはスターできない
+  def check_own_star
+    errors.add(:blog, ': 自分で書いたブログはスターできません。') if blog.user_id == user.id
+  end
+
+  # 下書き・限定公開・削除済みのブログはスターできない
+  def check_unreleased_blog
+    errors.add(:blog, ': 公開されていないブログはスターできません。') if blog.state_number_value != 2
+  end
 end
