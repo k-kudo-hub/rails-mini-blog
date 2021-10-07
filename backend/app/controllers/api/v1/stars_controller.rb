a501f0b0f7247de79a6db7bfc32ff4d90deef65a
k-kudo-hub <tongariisaibou@gmail.com>
2021-10-07 15:40:21 +0900

    It didn't work, so fixed it.

--- backend/app/controllers/api/v1/stars_controller.rb
+++ backend/app/controllers/api/v1/stars_controller.rb
@@ -1,27 +1,22 @@
 class Api::V1::StarsController < ApplicationController
-  before_action :set_blog, only: %i[create destroy]
   rescue_from ActiveRecord::RecordNotFound do |_exception|
     render json: { error: '404 not found' }, status: :not_found
   end
 
   def create
+    blog = Blog.find_by(url: params[:url])
     star = @current_user.stars.new(blog_id: blog.id)
     if star.save
       render status: :created
     else
-      render star.errors.full_messages
+      render json: star.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def destroy
+    blog = Blog.find_by(url: params[:url])
     star = @current_user.star.find_by(blog_id: blog.id)
     star.destroy!
     head :no_content
   end
-
-  private
-
-    def set_blog
-      blog = Blog.find_by(url: params[:url])
-    end
 end
